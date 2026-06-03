bfd9e622c2fbf0d8412bd293c05fb36701c00335
damian <damiangajdzinski45@gmail.com>
2026-06-03 14:55:01 +0200

    cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json({ limit: "10kb" }));
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Pozwól na żądania tylko z Twojego frontendu
+    origin: "costle.vercel.app", // Pozwól na żądania tylko z Twojego frontendu
     methods: ["GET", "POST"], // Określ dozwolone metody
     allowedHeaders: ["Content-Type"], // I nagłówki
   }),

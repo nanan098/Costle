8917e8538fc9b54a33562af3a7e81a5192bba538
damian <damiangajdzinski45@gmail.com>
2026-06-03 15:20:22 +0200

    vercel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header } from "./components/Header";
 import { Game } from "./components/Game";
+import { Analytics } from "@vercel/analytics/next";
 
 const App: React.FC = () => {
   return (
     <>
+      <Analytics />
       <Header />
       <Game />
     </>
